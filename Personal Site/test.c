7b449756031c9bf8d88773409e6257885bcefada
Lavanya Pushpakar <lavanya.pushpakar@outlook.com>
2022-03-17 22:50:36 +0530

    Add files via upload

---
+++ Personal Site/test.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+  char name[40];
+  printf("Enter your name:\n");
+  gets(name);
+
+  printf("is my Name");
+}
---
+++ intro-to-node/index.js
@@ -0,0 +1,7 @@
+var superheroes = require('superheroes');
+var superheroname = superheroes.random();
+console.log(superheroname);
+
+var supervillains = require('supervillains');
+var supervillainname =  supervillains.random();
+console.log(supervillainname);
